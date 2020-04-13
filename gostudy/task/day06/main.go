79d68d9d884da644ea18c58ee21b7060e1a136f1
haoyunpeng <haoyunpeng@hotmail.com>
2020-04-13 23:01:12 +0800

    ok

--- gostudy/task/day06/main.go
+++ gostudy/task/day06/main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"time"
-)
+import "fmt"
 
-func main() {
-	fmt.Println(time.Now().Format("2001/02/03-15:03:05"))
-	fmt.Println(os.Getgid())
+func main(){
+ 	fmt.Println("fmt")
 }
