c64338099260e55c800a5737e4086f12e017f9d7
oisnow <haoyunpeng@hotmail.com>
2020-10-14 23:34:48 +0800

    ok

---
+++ gostudy/cloudnativego/interface/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+type attacker struct {
+	apower int
+	hp     int
+}
+
+type sword struct {
+	attacker
+	color string
+}
+
+type gun struct {
+	attacker
+	short bool
+}
+
+func (at sword) wield() bool {
+	fmt.Println("this is a sowrd!")
+	return true
+}
+
+func (gu gun) wield() bool {
+	fmt.Println("this is a gun")
+	return true
+}
+
+type weapon interface {
+	wield() bool
+}
+
+func wielder(we weapon) bool {
+	fmt.Println("Wielding......")
+	return we.wield()
+}
+
+func main() {
+	gun1 := gun{attacker: attacker{apower: 10, hp: 100}, short: false}
+	sword1 := sword{attacker: attacker{apower: 9,
+		hp: 120}, color: "green"}
+	wielder(gun1)
+	wielder(sword1)
+}
