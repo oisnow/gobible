608bcc11f8489fa8e930ff85646a08d9534f4cf8
Hao <haoyunpeng@hotmail.com>
2020-10-16 20:51:39 +0800

    ok

--- gin-apls/apls.go
+++ gin-apls/apls.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		fmt.Print("pingpong")
 		c.JSON(200, gin.H{
 			"message": "pingpong",
 		})
+
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
