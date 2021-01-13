edac96d7fe263a6ae98fcb90a7fa4aa4f79859e1
Hao <haoyunpeng@hotmail.com>
2021-01-14 00:02:07 +0800

    迁移至goland

--- gin-apls/apls.go
+++ gin-apls/apls.go
@@ -10,11 +10,13 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		fmt.Print("pingpong")
+		pra := c.Param("name")
+		fmt.Println("name", pra)
 		c.JSON(200, gin.H{
 			"message": "pingpong",
 		})
 
 	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":8090") // listen and serve on 0.0.0.0:8080
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/Unknwon/goconfig v0.0.0-20200908083735-df7de6a44db8
-	github.com/go-stomp/stomp v2.1.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-stomp/stomp v2.1.0+incompatible
 )
